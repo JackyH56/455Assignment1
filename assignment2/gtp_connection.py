67224f69c758e46041162e45b32f60fe19efdce0
Jacky Huang <jacky_huang56@hotmail.com>
2022-02-12 22:04:07 -0700

    structure for genmove

--- assignment2/gtp_connection.py
+++ assignment2/gtp_connection.py
@@ -323,15 +323,22 @@ class GtpConnection:
         color = color_to_int(board_color)
         move = self.go_engine.get_move(self.board, color)
         if move is None:
-            self.respond('unknown')
+            self.respond('resign')
             return
-        move_coord = point_to_coord(move, self.board.size)
-        move_as_string = format_point(move_coord)
-        if self.board.is_legal(move, color):
-            self.board.play_move(move, color)
-            self.respond(move_as_string)
+        # if there is a move to play, try to use solver
         else:
-            self.respond("Illegal move: {}".format(move_as_string))
+            # response contains winner and move or unknown if time ran out
+            response = self.solve_cmd()
+            if response[0] == "b" or response[0] == "w":
+                move = response[1]
+            # use random move if response was unknown
+            move_coord = point_to_coord(move, self.board.size)
+            move_as_string = format_point(move_coord)
+            if self.board.is_legal(move, color):
+                self.board.play_move(move, color)
+                self.respond(move_as_string)
+            else:
+                self.respond("Illegal move: {}".format(move_as_string))
 
     def solve_cmd(self, args):
         # remove this respond and implement this method
