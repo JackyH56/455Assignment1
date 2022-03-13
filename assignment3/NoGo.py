c576d2d6322595d51e87df168dbd6d6cf334d9d1
Jacky Huang <jacky_huang56@hotmail.com>
2022-03-12 17:46:45 -0700

    part 1, rr, ucb, and random

--- assignment3/NoGo.py
+++ assignment3/NoGo.py
@@ -3,38 +3,138 @@
 # Set the path to your python3 above
 
 from gtp_connection import GtpConnection
-from board_util import GoBoardUtil
+from board_score import winner
+from board_util import GoBoardUtil, PASS
 from board import GoBoard
+from simulation_util import writeMoves, select_best_move
+from ucb import runUcb
+import numpy as np
+import argparse
+import sys
 
-
-class Go0:
-    def __init__(self):
+class Go3:
+    def __init__(self, move_select, sim_rule):
         """
-        NoGo player that selects moves randomly from the set of legal moves.
-
-        Parameters
-        ----------
-        name : str
-            name of the player (used by the GTP interface).
-        version : float
-            version number (used by the GTP interface).
+        Go player that selects moves by simulation.
         """
-        self.name = "Go0"
+        self.name = "Go3"
         self.version = 1.0
+        self.sim = 10
+        self.use_ucb = False if move_select == "rr" else True
+        self.random_simulation = True if sim_rule == "random" else False
+        self.use_pattern = not self.random_simulation
+
+    def simulate(self, board, move, toplay):
+        """
+        Run a simulated game for a given move.
+        """
+        cboard = board.copy()
+        cboard.play_move(move, toplay)
+        opp = GoBoardUtil.opponent(toplay)
+        return self.playGame(cboard, opp)
+
+    def simulateMove(self, board, move, toplay):
+        """
+        Run simulations for a given move.
+        """
+        wins = 0
+        for _ in range(self.sim):
+            result = self.simulate(board, move, toplay)
+            if result == toplay:
+                wins += 1
+        return wins
 
     def get_move(self, board, color):
-        return GoBoardUtil.generate_random_move(board, color, 
-                                                use_eye_filter=False)
+        """
+        Run one-ply MC simulations to get a move to play.
+        """
+        cboard = board.copy()
+        emptyPoints = board.get_empty_points()
+        moves = []
+        for p in emptyPoints:
+            if board.is_legal(p, color):
+                moves.append(p)
+        if not moves:
+            return None
+        moves.append(None)
+        if self.use_ucb:
+            C = 0.4  # sqrt(2) is safe, this is more aggressive
+            best = runUcb(self, cboard, C, moves, color)
+            return best
+        else:
+            moveWins = []
+            for move in moves:
+                wins = self.simulateMove(cboard, move, color)
+                moveWins.append(wins)
+            writeMoves(cboard, moves, moveWins, self.sim)
+            return select_best_move(board, moves, moveWins)
 
+    def playGame(self, board, color):
+        """
+        Run a simulation game.
+        """
+        nuPasses = 0
+        for _ in range(self.limit):
+            color = board.current_player
+            if self.random_simulation:
+                move = GoBoardUtil.generate_random_move(board, color, False)
+            else:
+                # TODO make pattern util file that generates pattern moves with weight.txt file
+                move = PatternUtil.generate_pattern_moves(
+                    board, self.use_pattern
+                )
+            board.play_move(move, color)
+            if move == PASS:
+                nuPasses += 1
+            else:
+                nuPasses = 0
+            if nuPasses >= 2:
+                break
+        return winner(board)
 
-def run():
+def run(move_select, sim_rule):
     """
-    start the gtp connection and wait for commands.
+    Start the gtp connection and wait for commands.
     """
     board = GoBoard(7)
-    con = GtpConnection(Go0(), board)
+    con = GtpConnection(Go3(move_select, sim_rule), board)
     con.start_connection()
 
 
+def parse_args():
+    """
+    Parse the arguments of the program.
+    """
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--moveselect",
+        type=str,
+        default="rr",
+        help="type of move selection: rr or ucb",
+    )
+    parser.add_argument(
+        "--simrule",
+        type=str,
+        default="random",
+        help="type of simulation policy: random or patternbased",
+    )
+
+    args = parser.parse_args()
+    sim = args.sim
+    move_select = args.moveselect
+    sim_rule = args.simrule
+    if move_select != "rr" and move_select != "ucb":
+        print("moveselect must be rr or ucb")
+        sys.exit(0)
+    if sim_rule != "random" and sim_rule != "patternbased":
+        print("simrule must be random or patternbased")
+        sys.exit(0)
+
+    return move_select, sim_rule
+
+
 if __name__ == "__main__":
-    run()
+    move_select, sim_rule = parse_args()
+    run(move_select, sim_rule)
