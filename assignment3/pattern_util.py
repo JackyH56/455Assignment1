d04135711b26546709dfeafa98c265dd693e5d69
Chidubem Ogbudibe <45117618+Dubem101@users.noreply.github.com>
2022-03-14 00:52:16 -0600

    Update pattern_util.py

--- assignment3/pattern_util.py
+++ assignment3/pattern_util.py
@@ -1,5 +1,5 @@
 import csv
-from board_util import GoBoardUtil
+from board_util import GoBoardUtil, PASS
 from board_util import coord_to_point
 from gtp_connection import move_to_coord
 import random
@@ -13,35 +13,42 @@ for entry in reader:
 class PatternUtil(object):
     @staticmethod
     def generate_pattern_moves(board, color):
+        # distribution_item[0] - the move
+        # distribution_item[1] - the board code
+        # distribution_item[2] - the probability
         legal_moves = GoBoardUtil.generate_legal_moves(board, color)
         distribution = []
         sum = 0.0
-        for move in legal_moves:
-            code = PatternUtil.get_pattern_code(move, board)
-            if weights[code] and weights[code] != 1.0:
-                distribution.append((code, weights[code]))
-                sum += weights[code]
+        for point in legal_moves:
+            code = PatternUtil.get_pattern_code(point, board)
+            if weights[code] and weights[code][1] != 1.0:
+                distribution.append([point, code, weights[code][1]])
+                sum += weights[code][1]
         
         # adjust probabilities so they add up to 1, a proper distribution
         for pattern in distribution:
-            pattern[1] = pattern[1] / sum
+            pattern[2] = pattern[2] / sum
 
         return distribution
 
     @staticmethod
     def random_select(distribution):
+        # item[0] - the move
+        # item[1] - the board code
+        # item[2] - the probability
+        if len(distribution) == 0:
+            return PASS
+
         r = random.random()
         sum = 0.0
         for item in distribution:
-            sum += item[1]
+            sum += item[2]
             if sum > r:
-                return item
-        return distribution[-1]
+                return item[0]
+        return distribution[-1][0]
 
     @staticmethod
-    def get_pattern_code(move, board):
-        coord = move_to_coord(str(move), board.size)
-        point = coord_to_point(coord[0], coord[1], board.size)
+    def get_pattern_code(point, board):
         adjacent = [
             point - board.NS - 1,
             point - board.NS,
@@ -54,6 +61,6 @@ class PatternUtil(object):
         
         code = 0
         for i in range(8):
-            code += board[adjacent[i]] * 4 ^ i
+            code += board.get_color(adjacent[i]) * 4 ^ i
 
         return code
